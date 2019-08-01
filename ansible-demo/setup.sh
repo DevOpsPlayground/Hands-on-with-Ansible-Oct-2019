2af7ee50c7be613b69f61af5d227d94c572712b1
meta-morpho-sys <yuliya.nedyalkova777@gmail.com>
2019-08-01 20:35:03 +0100

    added copying ssh-id to auth_keys on remote: added path to Python3

--- ansible-demo/setup.sh
+++ ansible-demo/setup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+host=${1}
+
 if [ $# -eq 0 ]
   then
     echo "No arguments supplied"
 fi
 
 mkdir -p ~/ansible-demo && cd ~/ansible-demo
-
-echo ${1} > ansible_inventory && 
-echo "inventory = ansible_inventory" > ansible.cfg
+echo ${host} > ansible_inventory && echo -e "[inventory]\ninventory = ansible_inventory\nansible_path=/usr/local/bin/python3" > ansible.cfg
+ssh-copy-id -i ~/.ssh/id_rsa.pub ubuntu@${host}
