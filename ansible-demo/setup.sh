f3dd56f45a6b58c2dcdeefbd924713651f4e3b84
meta-morpho-sys <yuliya.nedyalkova777@gmail.com>
2019-08-01 22:20:23 +0100

    Added a header to the invetory file

--- ansible-demo/setup.sh
+++ ansible-demo/setup.sh
@@ -9,5 +9,5 @@ if [ $# -eq 0 ]
 fi
 
 mkdir -p ~/ansible-demo && cd ~/ansible-demo
-echo ${host} > ansible_inventory && echo -e "[inventory]\ninventory = ansible_inventory\nansible_path=/usr/local/bin/python3" > ansible.cfg
+echo -e "[db_hosts]\n${host}" > ansible_inventory && echo -e "[inventory]\ninventory = ansible_inventory\nansible_path=/usr/local/bin/python3" > ansible.cfg
 ssh-copy-id -i ~/.ssh/id_rsa.pub ubuntu@${host}
