a65ffb577bf8b2359c196dd0002c8aa81e7e306d
meta-morpho-sys <yuliya.nedyalkova777@gmail.com>
2019-08-02 14:17:49 +0100

    Last version

---
+++ ansible-yu-demo/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+host=${1}
+
+if [ $# -eq 0 ]
+  then
+    echo -e "Remote host ip required.\nUsage:./setup.sh 34.244.168.125"
+    exit
+fi
+
+mkdir -p ~/ansible-demo && cd ~/ansible-demo
+echo -e "[db_hosts]\n${host}" > ansible_inventory && echo -e "[defaults]\ninventory = ansible_inventory\nansible_path=/usr/local/bin/python3" > ansible.cfg
+cd -
+
+# set ssh connection to your host
+ssh-add "yuliya_ans.pem"
+ssh-copy-id -i ~/.ssh/id_rsa.pub ubuntu@${host}
