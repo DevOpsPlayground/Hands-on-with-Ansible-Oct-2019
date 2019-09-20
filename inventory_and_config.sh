bbd4f4c2ea21f28a588deb69fe71128466feb440
meta-morpho-sys <yuliya.nedyalkova777@gmail.com>
2019-09-20 18:49:16 +0100

    Creating inventory and config

---
+++ inventory_and_config.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+host=${1}
+
+if [ $# -eq 0 ]
+  then
+    echo -e "Remote host ip required.\nUsage:./create_inventory_and_config.sh 34.244.168.125"
+    exit
+fi
+
+echo -e "[web]\n${host}" > ./ansible_hands_on/ansible_inventory && echo -e "[defaults]\ninventory = ansible_inventory\nansible_path=/usr/local/bin/python3" > ./ansible_hands_on/ansible.cfg
