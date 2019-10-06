d0d205c39ed847f07dc713a55827d935f55c97c4
meta-morpho-sys <yuliya.nedyalkova777@gmail.com>
2019-10-06 17:47:08 +0100

    Tidying up

--- inventory_and_config.sh
+++ inventory_and_config.sh
@@ -4,7 +4,7 @@ host=${1}
 
 if [ $# -eq 0 ]
   then
-    echo -e "Remote host ip required.\nUsage:./create_inventory_and_config.sh 34.244.168.125"
+    echo -e "Remote host ip required.\nUsage:./inventory_and_config.sh 34.244.168.125"
     exit
 fi
 
