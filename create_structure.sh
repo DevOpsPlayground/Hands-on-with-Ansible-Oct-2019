c7492193a172e7b7e01fc3298f19f8726f7c728e
meta-morpho-sys <yuliya.nedyalkova777@gmail.com>
2019-10-13 22:34:15 +0100

    Helper

---
+++ create_structure.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir -p roles/common/tasks
+mkdir -p roles/web/tasks
+mkdir -p roles/web/handlers
+mkdir -p roles/web/templates
+mkdir -p roles/web/vars
+mkdir -p roles/db/tasks
+mkdir -p roles/db/handlers
+mkdir -p roles/db/vars
+mkdir -p roles/php/tasks
+mkdir group_vars
+echo Creating directory structure for your playbook
+echo Done!
